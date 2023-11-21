d8735acd0a682c07913fb04f61f6d30207b68e58
Matheus Soares Duarte <matheus.sd3210@gmail.com>
2023-11-21 16:24:28 -0300

    renomeação de pastas

--- Unidade-1/arrays-e-loops/exercicio-2.js
+++ Unidade-1/arrays-e-loops/exercicio-2.js
@@ -2,5 +2,5 @@
  const array = ['a', 'b', 'c', 'd', 'e', 'f'];
 
  for (index= 0; index < array.length; index ++){
-    console.log(array[index]);
+    console.log(array[index]); //poderia ter sido feito com while.
  }
