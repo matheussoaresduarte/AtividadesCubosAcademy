6ab2e95a15a904fc98337cbb4c5fc7885a7833d3
Matheus Soares Duarte <matheus.sd3210@gmail.com>
2023-11-21 16:01:54 -0300

    exercicios sobre arrays

---
+++ Unidade-1/arrays/exercicio-1.js
@@ -0,0 +1,25 @@
+
+// crie um array com 5 nomes de paises
+const nomeDepaises = ['brasil', 'argentina', 'paraguai', 'colombia', 'canada'];
+// adicione um pais ao fim da lista
+nomeDepaises.push('italia');
+// imprima a lista na tela
+console.log(nomeDepaises);
+// remova um pais do fim da lista
+nomeDepaises.pop();
+// imprima a lista na tela
+console.log(nomeDepaises);
+// adicione um pais ao inicio da lista
+nomeDepaises.unshift('russia');
+// imprima a lista na tela
+console.log(nomeDepaises);
+// remova o pais do inicio da lista
+nomeDepaises.shift();
+// imprima a lista na tela
+console.log(nomeDepaises);
+// imprima o utimo pais da lista na tela
+console.log(nomeDepaises[nomeDepaises.length - 1]);
+// imprima o segundo pais da lista na tela
+console.log(nomeDepaises[1]);
+// imprima o pais de indice 2 na tela 
+console.log(nomeDepaises[2]);
