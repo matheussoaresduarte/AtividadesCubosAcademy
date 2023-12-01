f6925c169ce7988992f10703c6cff7b527265e0b
Matheus Soares Duarte <matheus.sd3210@gmail.com>
2023-12-01 14:40:47 -0300

    exercicio 4 objetos

---
+++ Unidade-1/objetos/exercicio-4.js
@@ -0,0 +1,32 @@
+
+let totalConta = 0;
+
+const cartao = {
+    nomeDoCliente: "matheus",
+    idadeDoCliente: 23,
+    produtosConsumidos: [
+         {
+        nomeDoProduto: "pao integral",
+        precoUnitario: 0.25,
+        quantidade: 5,
+        },
+            
+        {
+        nomeDoProduto: "pneu",
+        precoUnitario: 30.00,
+        quantidade: 2,
+        },
+        
+        {
+        nomeDoProduto: "chinelo",
+        precoUnitario: 17.90,
+        quantidade: 3,
+        },
+    ],
+};
+
+for( let produto of cartao.produtosConsumidos){
+     totalConta += produto.precoUnitario * produto.quantidade;
+}
+
+console.log(`Ola, ${cartao.nomeDoCliente} a sua fatura chegou, o valor total a ser pago é de: ${totalConta.toFixed(2)}`);
