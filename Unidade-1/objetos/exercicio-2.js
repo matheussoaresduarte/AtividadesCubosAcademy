613e84b8791fd58e6d2a93fe156e28a354c4cb02
Matheus Soares Duarte <matheus.sd3210@gmail.com>
2023-12-01 14:15:59 -0300

    exercicio 2 sobre objetos

---
+++ Unidade-1/objetos/exercicio-2.js
@@ -0,0 +1,23 @@
+const cartao = {
+    nomeDoCliente: "matheus",
+    idadeDoCliente: 23,
+    produtosConsumidos: [
+         {
+        nomeDoProduto: "pao integral",
+        precoUnitario: 0.25,
+        quantidade: 5,
+        },
+            
+        {
+        nomeDoProduto: "pneu",
+        precoUnitario: 30.00,
+        quantidade: 2,
+        },
+        
+        {
+        nomeDoProduto: "chinelo",
+        precoUnitario: 17.90,
+        quantidade: 3,
+        },
+    ],
+}
