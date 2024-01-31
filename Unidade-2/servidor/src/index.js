7109d7b4532e98008faae7d309d784d656632df8
Matheus Soares <matheus.sd3210@gmail.com>
2024-01-31 11:31:57 -0300

    teste primeiro servidor concluido

--- Unidade-2/servidor/src/index.js
+++ Unidade-2/servidor/src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    app.send("olá, seja bem vindo ao meu primeiro servidor.");
+    res.send("olá, seja bem vindo ao meu primeiro servidor.");
 });
 
 app.listen(3000);
