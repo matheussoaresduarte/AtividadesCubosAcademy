c7460630694104c0798be38711523f79e61e7202
Matheus Soares Duarte <matheus.sd3210@gmail.com>
2024-04-11 10:08:13 -0300

    linkando node com o banco de dados com pg

--- Unidade-3/index.js
+++ Unidade-3/index.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const {Pool } = require('pg');
 
 const app = express();
 
+const pool = new Pool({
+    host: 'localhost',
+    port: 5432,
+    user: 'postgres',
+    password: '06041954',
+    database: 'aula_conexao_node_pg'
+});
+
 app.use(express.json());
 
-app.get ('/', (req, res) => {
-    return res.json('tudo certo')
+app.get ('/', async (req, res) => {
+    
+    try {
+        
+        const resultado = await pool.query('SELECT * FROM empresas');
+
+        return res.json(resultado);
+    } catch (error) {
+        console.log(error.message);
+    }
 });
 
 app.listen(3000);
